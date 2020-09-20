b79869faaf0453929c8d4254ded22c92173d9c25
na0070 <na0070@mix.wvu.edu>
2020-09-20 17:32:27 -0400

    php + login

---
+++ signup.php
@@ -0,0 +1,50 @@
+<?php
+require "includes/header.php"
+?>
+
+<main>
+    
+    <div class="bg-cover">
+        <div class="h-100 container center-me">
+            <div class="my-auto">
+                <div class="signup-form">
+                    <form action="includes/signup-helper.php" method="post">
+                        <h2>Register</h2>
+                        <p class="hint-text">Create your account now!</p>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" name="fname" placeholder="First name" required="required">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="lname" placeholder="Last name" required="required">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="uname" placeholder="Username" required="required">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" name="email" placeholder="email address" required="required">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="pwd" placeholder="Password" required="required">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="con-pwd" placeholder="Confirm password" required="required">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="signup-submit" class="btn btn-outline-dark btn-lg btn-block">Submit</button>
+                        </div>
+                    </form>
+
+                    <div class="text-center">Already a member? <a href="login.html">Sign in!</a></div>
+
+
+                </div>
+            </div>
+        </div>
+    
+    </div>
+
+</main>
